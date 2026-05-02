f74d53d6046c7a98582cc64ac199968744c5631b
Serveaux Tao <taoserveauxx@gmail.com>
2026-05-02 16:14:52 +0200

    -upgrade exchange

---
+++ src/core/exchange.py
@@ -0,0 +1,47 @@
+import ccxt
+
+class Exchange:
+    
+    def __init__(self, apiKey, secretKey, passPhrase):  
+        
+        self.__exchange = ccxt.bitget({
+            'apiKey' : apiKey,
+            'secret' : secretKey,
+            'password' : passPhrase,
+            'options' : {
+                'defaultType' : 'swap'
+            }
+        })
+                
+        self.__pairsPrices = {}
+
+    
+    def checkPrice(self, pairList):
+        
+        for i in pairList :
+            price = self.__exchange.fetch_ticker(f'{i}/USDT:USDT')
+            self.__pairsPrices[i] = price
+        
+        
+        
+    
+    def lever(self):
+        pass
+    
+    def setTakeProfit(self, takeProfit):
+        pass
+    
+    def setStopLoss(self, stopLoss):
+        pass
+    
+    def balanceAvalaible(self):
+        pass
+    
+    def currentPos(self):
+        pass
+    
+    def openPos(self, direction):
+        pass
+    
+    def closePos(self):
+        pass
