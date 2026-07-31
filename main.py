ec98e4ccb0c97c02cf95fa3d2926e0214bb7134a
Serveaux Tao <taoserveauxx@gmail.com>
2026-07-31 21:36:57 +0200

    error

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ secretKey = os.getenv('KRAKEN_SECRET_KEY')
 telegramChatId = os.getenv('TELEGRAM_CHAT_ID')
 telegramToken = os.getenv('TELEGRAM_TOKEN')
 
-symbol = symbol = os.getenv('SYMBOL', 'TAO/USD:USD')
+symbol = os.getenv('SYMBOL', 'TAO/USD:USD')
 
 if __name__ == '__main__':
     combos = {'MACDxRSIxMFI' : [StrategieMACD,StrategieRSI,StrategyMFI],
